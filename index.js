5201f2fbf70a61720c5623feba9327d683fda86f
Matias Reyes <matias_reyes@icloud.com>
2022-12-27 12:39:21 -0300

    recursive game method, need to pull out fetch function so numbers dont refetch

--- index.js
+++ index.js
@@ -13,17 +13,29 @@ const promptSchema = {
   }
 }
 
-const masterMind = async () => {
-  let tries = 1
+const masterMind = async (tries) => {
+  if (tries === 10) {
+    console.log('GAME OVER')
+    return
+  }
+
   // Parsing numbers from API to array of ints
   const randomNumbers = await axios.get(baseUrl)
   const numbers = randomNumbers.data
   const newNumbers = numbers.split('\n')
   newNumbers.splice(newNumbers.length - 1)
+  console.log(newNumbers)
 
   // Fetch first guess from user
   console.log(`Enter guess #${tries}`)
   const { userGuess } = await prompt.get(promptSchema)
+
+  // Parse user response to int
+  const guess = userGuess.split(', ')
+
+  // Check if guess is correct
+  masterMind(tries + 1)
 }
 
-masterMind()
+const tries = 1
+masterMind(tries)

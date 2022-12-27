ce6e623463717cb132cda5a4078fec8c1dee3a5e
Matias Reyes <matias_reyes@icloud.com>
2022-12-27 08:55:00 -0300

    number array parse

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const axios = require('axios')
 
 const baseUrl = 'https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain'
 
-axios.get(baseUrl)
-  .then((response) => console.log(response.data))
+const masterMind = async () => {
+  // Parsing numbers from API to array of ints
+  const randomNumbers = await axios.get(baseUrl)
+  const numbers = randomNumbers.data
+  const newNumbers = numbers.split('\n')
+  newNumbers.splice(newNumbers.length - 1)
+}
+
+masterMind()

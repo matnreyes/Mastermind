5110f34745161ea215c1fa20538b8422d8dd7cfe
Matias Reyes <matias_reyes@icloud.com>
2022-12-28 09:53:49 -0300

    working guess route

--- controllers/guess.js
+++ controllers/guess.js
@@ -1,9 +1,22 @@
 const guessRouter = require('express').Router()
 
 guessRouter.post('/', async (req, res) => {
-  const turn = req.body
+  const { secretCode, guess } = req.body
+  console.log(typeof(guess))
 
-  res.status(201).json(turn)
+  // Check if number and/or placement are correct
+  let correctGuess = 0
+  let correctLocation = 0
+  guess.forEach((number, index) => {
+    if (secretCode.includes(number)) {
+      correctGuess += 1
+      if (number === secretCode[index]) {
+        correctLocation += 1
+      }
+    }
+  })
+
+  res.json({ number: correctGuess, location: correctLocation})
 })
 
 module.exports = guessRouter

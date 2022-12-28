a4de016887f91667889cc681dfb9af965509d236
Matias Reyes <matias_reyes@icloud.com>
2022-12-27 22:00:18 -0300

    game functionality

--- index.js
+++ index.js
@@ -13,29 +13,64 @@ const promptSchema = {
   }
 }
 
-const masterMind = async (tries) => {
-  if (tries === 10) {
-    console.log('=====  GAME OVER  =====')
+const fetchNumbers = async () => {
+  // Fetch random numbers from API
+  const randomNumbers = await axios.get(baseUrl)
+  const numbers = await randomNumbers.data
+
+  // Parse numbers into array
+  const newNumbers = numbers.split('\n')
+  const code = newNumbers.slice(0, 4)
+  return code
+}
+
+const masterMind = async (tries, secretCode) => {
+  // Losing case
+  if (tries > 10) {
+    console.log('========  GAME OVER  ========')
     return
   }
 
-  // Parsing numbers from API to array of ints
-  const randomNumbers = await axios.get(baseUrl)
-  const numbers = randomNumbers.data
-  const newNumbers = numbers.split('\n')
-  newNumbers.splice(newNumbers.length - 1)
-  console.log(newNumbers)
-
   // Fetch first guess from user
-  console.log(`Enter guess #${tries}`)
+  console.log(`Enter guess #${tries}:`)
   const { userGuess } = await prompt.get(promptSchema)
 
   // Parse user response to int
   const guess = userGuess.split(', ')
 
+  // Logic for checking correctness
+  let correctNumber = 0
+  let correctLocation = 0
+  guess.forEach((number, index) => {
+    if (secretCode.includes(number)) {
+      correctNumber += 1
+
+      if (number === secretCode[index]) {
+        correctLocation += 1
+      }
+    }
+  })
+
+  // Give hint to user
+  console.log(`${correctNumber} correct numbers and ${correctLocation} correct locations`)
+
+  // Winning case
+  if (correctLocation === 4) {
+    console.log('==========================')
+    console.log('         YOU WON!!!       ')
+    console.log('==========================')
+    return
+  }
+
   // Check if guess is correct
-  masterMind(tries + 1)
+  masterMind(tries + 1, secretCode)
 }
 
-const tries = 1
-masterMind(tries)
+// Main game function
+const main = async () => {
+  const secretCode = await fetchNumbers()
+  const tries = 1
+  masterMind(tries, secretCode)
+}
+
+main()

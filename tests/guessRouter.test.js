6c65821939398d2079cc0e3ab5c88bf1a72d2bdb
Matias Reyes <matias_reyes@icloud.com>
2022-12-30 14:38:22 -0300

    add guess scenarios
    added new scenarios for testing of guessRouter

--- tests/guessRouter.test.js
+++ tests/guessRouter.test.js
@@ -4,8 +4,8 @@ const app = require('../app')
 const api = supertest(app)
 
 describe('Guess is checked', () => {
+  const secretCode = [1, 2, 3, 4]
   test('and has all the same number', async () => {
-    const secretCode = [1, 2, 3, 4]
     const guess = [1, 1, 1, 1]
 
     const guessResponse = await api
@@ -18,5 +18,47 @@ describe('Guess is checked', () => {
 
     expect(results.location).toEqual(1)
     expect(results.digit).toEqual(1)
+  })
+
+  test('and has no correct guesses', async () => {
+    const guess = [5, 6, 7, 0]
+
+    const guessResponse = await api
+      .post('/api/guess')
+      .send({ secretCode, guess })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const results = guessResponse.body
+    expect(results.location).toEqual(0)
+    expect(results.digit).toEqual(0)
+  })
+
+  test('and all numbers are right but wrong place', async () => {
+    const guess = [4, 3, 2, 1]
+
+    const guessResponse = await api
+      .post('/api/guess')
+      .send({ secretCode, guess })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const results = guessResponse.body
+    expect(results.location).toEqual(0)
+    expect(results.digit).toEqual(4)
+  })
+
+  test('and all numbers are correct and in the right location', async () => {
+    const guess = [1, 2, 3, 4]
+
+    const guessResponse = await api
+      .post('/api/guess')
+      .send({ secretCode, guess })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const results = guessResponse.body
+    expect(results.location).toEqual(4)
+    expect(results.digit).toEqual(4)
   })
 })

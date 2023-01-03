33c25cfb75092c8c85f53e24e1a29879afd59dc9
Matias Reyes <matias_reyes@icloud.com>
2023-01-03 14:39:53 -0300

    added boilerplate for create user router

---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+const userRouter = require('express').Router()
+const bcryot = require('bcrypt')
+const User = require('../models/user')
+
+userRouter.post('/', async (req, res) => {
+
+})
+
+module.exports = userRouter

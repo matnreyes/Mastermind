80dedc7392815718197c0cb7087d97dd590d9218
Matias Reyes <matias_reyes@icloud.com>
2023-01-03 17:51:46 -0300

    userLogin router

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,17 @@
 const userRouter = require('express').Router()
-const bcryot = require('bcrypt')
+const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 userRouter.post('/', async (req, res) => {
+  const { username, password } = req.body
+  const passwordHash = bcrypt.hash(password, 10)
 
+  const user = new User({
+    username,
+    passwordHash
+  })
+  const savedUser = await user.save()
+  res.status(201).json(savedUser)
 })
 
 module.exports = userRouter

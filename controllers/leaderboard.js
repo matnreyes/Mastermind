eeef5d4979a3fbeaa4d571d3911984fb71aed4a9
matnreyes <matias_reyes@icloud.com>
2023-01-07 12:39:11 -0500

    finished leaderboard route

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -2,10 +2,24 @@ const leaderboardRouter = require('express').Router()
 const User = require('../models/user')
 
 leaderboardRouter.get('/', async (req, res) => {
-  const users = await User.findMany({})
+  /*
+    Fetches all users from database
+    Sorts by wins
+    Returns only top ten
+    Cleans array so only username and wins are returned, no userID
+    (passwordHash is removed in conversion to JSON)
+  */
+  const users = await User.find({})
   users.sort((a, b) => b.wins - a.wins)
-  console.log(users)
-  res.json()
+  const topTen = users.slice(0, 9)
+  const leaderboard = topTen.map(p => {
+    const player = {
+      username: p.username,
+      wins: p.wins
+    }
+    return player
+  })
+  res.json(leaderboard)
 })
 
 module.exports = leaderboardRouter

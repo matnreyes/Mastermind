d1fdf24548d6fc40a1f6960b065dd827cebae81a
matnreyes <matias_reyes@icloud.com>
2023-01-07 20:50:16 -0500

    added new production build ;

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#53d166",
+          "secondary": "#a02e11",
+          "accent": "#195f8e",
+          "neutral": "#2E263B",
+          "base-100": "#252145",
+          "info": "#62B2F4",
+          "success": "#49D4AF",
+          "warning": "#ECA355",
+          "error": "#F43444",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
